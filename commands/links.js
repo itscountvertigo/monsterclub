ed8462e8b32d4cfae9ca5bef37a76b23edd06152
Anbiya Popal <55081188+itscountvertigo@users.noreply.github.com>
2020-07-06 21:01:32 +0200

    fix bug with switch statement, now if-elif structure

--- commands/links.js
+++ commands/links.js
@@ -4,30 +4,26 @@ module.exports = {
     execute(message, args){
         const Discord = require('discord.js');
         
-        switch(args[0]){
-            case 'mc' || 'monsterclub':
-                const embed = new Discord.MessageEmbed()
+        if(args[0] === 'mc' || 'monsterclub'){
+            const embed = new Discord.MessageEmbed()
                 .setTitle('The Monster Club Corporation')
                 .setURL('https://www.themonsterclubcorporation.com/')
                 .setColor(0xcb0063);
                 message.channel.send(embed);
-                break;
-            
-            case 'shop' || 'store':
-                const embed = new Discord.MessageEmbed()
+        } 
+        else if(args[0] === 'shop' || 'store') {
+            const embed = new Discord.MessageEmbed()
                 .setTitle('The Monster Club Corporation Shop')
                 .setURL('https://www.themonsterclubcorporation.com/shop/')
                 .setColor(0xcb0063);
                 message.channel.send(embed);
-                break;
-
-            case 'merch':
-                const embed = new Discord.MessageEmbed()
+        }
+        else if(args[0] === 'merch'){
+            const embed = new Discord.MessageEmbed()
                 .setTitle('Monster Club Merchandise - Pharoah Tom')
                 .setURL('https://pharoahtomscollections.com/collections/monster-club')
                 .setColor(0xcb0063);
                 message.channel.send(embed);
-                break;
         }
     }
 }
