cac484cbe4cd3f3d54682f568d1e4f7eee67a8ad
Anbiya Popal <55081188+itscountvertigo@users.noreply.github.com>
2020-07-06 20:54:17 +0200

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,7 @@ module.exports = {
         .setTitle('Commands')
         .addField("!card [card number (eg 'S0-01')]", 'Gives information on specific card (name, type, rock/paper/scissors, how much power, how much it can stack, etc.). It only works if the S is uppercase.')
         .addField('!help or !commands', 'Shows available commands.')
+        .addField('!link [shop/store, mc/monsterclub, OR merch] or !url [shop/store, mc/monsterclub, OR merch]', 'Pick between shop/store, mc/monsterclub, or merch. Will answer with link there.')
 
         .setColor(0xcb0063)
 
